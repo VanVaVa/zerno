2686ecb1baffa06b56862bd6aa9eb87fa8078cd8
Van <MisterDeterminator@yandex.ru>
2025-10-03 14:45:00 +0000

    feat: added header

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.scss";
+import Header from "@/widgets/header/Header";
+
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  subsets: ["cyrillic", "latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Зерно",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ru">
+      <body className={`${montserrat.variable}`}>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/widgets/header/Header.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import styles from "./header.module.scss";
+import Link from "next/link";
+
+const Header = () => (
+  <header className={styles.header}>
+    <Link href="/">
+      <Image src="/images/logo.svg" width={174} height={50} alt="зерно" />
+    </Link>
+    <ul className={styles.list}>
+      <li>
+        <Link href="/">О нас</Link>
+      </li>
+      <li>
+        <Link href="/">Услуги</Link>
+      </li>
+      <li>
+        <Link href="/">WOW-парк</Link>
+      </li>
+      <li>
+        <Link href="/">Портфолио</Link>
+      </li>
+      <li>
+        <Link href="/">Контакты</Link>
+      </li>
+      <li className={styles.filled}>
+        <Link href="/">Заказать услугу</Link>
+      </li>
+    </ul>
+  </header>
+);
+
+export default Header;
